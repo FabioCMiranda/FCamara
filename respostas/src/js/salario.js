1c0c254845efaa0010beec8b4deb27a4dc47cb51
Fabio Cavalcanti de Miranda <fabio.c.miranda@hotmail.com>
2018-08-23 23:16:55 -0300

    Correção
    Ajustes de nomes e comentários.

--- respostas/src/js/salario.js
+++ respostas/src/js/salario.js
@@ -1,20 +1,18 @@
 function calcular()
 {   
     var salarioBase = 1000;
-    var salario = salarioBase * 1.015;
+    var salario = salarioBase * 1.015;          // Salário no ano 2006
     var salAtual = 0;
     var tax = 0.015;
     var anoAtual = document.getElementById("ano").value;
-    while(anoAtual >= 2007)
-    {
+    while(anoAtual >= 2007)                     // Loop para cálculo da reajuste
+    {                                           // salarial
         tax *= 2;
         salario = salarioBase + (salarioBase * tax);
         anoAtual--;
         salAtual = salario;
-        alert(tax);
     }
 
-    //document.querySelector("#salarioAtual").value = salAtual;
     salAtual = parseFloat(salAtual);
     document.getElementById("salarioAtual").value = salAtual.toFixed(2);
     
